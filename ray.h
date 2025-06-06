518ab652151deeb6cf5d0e3063e3aaddf714db7f
Justin <o0justinsgamez0o@gmail.com>
2025-06-06 14:06:47 -0400

    feat: Send rays into a scene

---
+++ ray.h
@@ -0,0 +1,25 @@
+#ifndef RAY_H
+#define RAY_H
+
+#include "vec3.h"
+
+class ray {
+  public:
+    ray() {}
+
+    ray(const point3& origin, const vec3& direction) : orig(origin), dir(direction) {}
+
+    // Returns a reference to a private memebr of the class, wehre cosnt means it cannot be modified by the function
+    const point3& origin() const  { return orig; }
+    const vec3& direction() const { return dir; }
+
+    point3 at(double t) const {
+        return orig + t*dir;
+    }
+
+  private:
+    point3 orig;
+    vec3 dir;
+};
+
+#endif
